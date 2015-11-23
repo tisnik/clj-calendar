d65f28af7f3b8f6d2db14e3cff9ee9a91721e663
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-11-23 20:11:38 +0100

    Added licence text into all source files

--- project.clj
+++ project.clj
@@ -1,3 +1,15 @@
+;
+;  (C) Copyright 2015  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 (defproject clj-calendar "0.1.0-SNAPSHOT"
   :description "Set of functions for manipulating with Calendar(s)"
   :url "http://example.com/FIXME"
