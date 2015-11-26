f65d21cf1004bb45349e0ee4659c0335ff2b513f
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-11-26 22:33:15 +0100

    New functions: get-calendar, format-date-using-desired-format and format-date-time

--- src/clj_calendar/calendar.clj
+++ src/clj_calendar/calendar.clj
@@ -10,5 +10,22 @@
 ;      Pavel Tisnovsky
 ;
 
-(ns clj-calendar.calendar)
+(ns clj-calendar.calendar
+    "Namespace that contains various functions for handling calendar and dates.")
+
+(defn get-calendar
+    "Gets an instance of calendar using the default time zone and default locale."
+    []
+    (java.util.Calendar/getInstance))
+
+(defn format-date-using-desired-format
+    "Format given date using desired format, for example 'yyyy-MM-dd' etc."
+    [calendar desired-format]
+    (let [date-format (new java.text.SimpleDateFormat desired-format)]
+        (.format date-format (.getTime calendar))))
+
+(defn format-date-time
+    "Format given date using the following format: 'yyyy-MM-dd HH:mm:ss'"
+    [calendar]
+    (format-date-using-desired-format calendar "yyyy-MM-dd HH:mm:ss"))
 
