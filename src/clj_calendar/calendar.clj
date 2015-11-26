0fa05eb9558a6a4684b03433505a1566a453db35
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-11-26 22:38:32 +0100

    Four new functions: get-calendar-for-week, get-year, get-week, and get-day-of-week

--- src/clj_calendar/calendar.clj
+++ src/clj_calendar/calendar.clj
@@ -18,6 +18,30 @@
     []
     (java.util.Calendar/getInstance))
 
+(defn get-calendar-for-week
+    "Gets a calendar for given year and week."
+    [year week]
+    (let [cal (get-calendar)]
+        (.clear cal)
+        (.set cal java.util.Calendar/WEEK_OF_YEAR week)
+        (.set cal java.util.Calendar/YEAR year)
+        cal))
+
+(defn get-year
+    "Returns the year for the current date."
+    [calendar]
+    (.get calendar java.util.Calendar/YEAR))
+
+(defn get-week
+    "Returns week number within the current year."
+    [calendar]
+    (.get calendar java.util.Calendar/WEEK_OF_YEAR))
+
+(defn get-day-of-week
+    "Returns day of week for given date."
+    [calendar]
+    (.get calendar java.util.Calendar/DAY_OF_WEEK))
+
 (defn format-date-using-desired-format
     "Format given date using desired format, for example 'yyyy-MM-dd' etc."
     [calendar desired-format]
