4d7264d5516258b64fc84c3cf9a76be9d501d28f
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-01-06 21:10:11 +0100

    New functions: format-time and format-time-

--- src/clj_calendar/format_datetime.clj
+++ src/clj_calendar/format_datetime.clj
@@ -13,3 +13,21 @@
 (ns clj-calendar.format-datetime
     "Namespace that contains various functions for handling calendar and dates.")
 
+(defn format-time
+    "Format a given time unit into human readable format."
+    [amount unit]
+    (format "%d %s ago" amount unit))
+
+(defn format-time-
+    "Format time into human readable format."
+    [amount1 amount2 unit1 unit2]
+    (if (= amount1 1)
+        (cond
+            (= amount2 0) (format "one %s ago" unit1)
+            (= amount2 1) (format "one %s and one %s ago" unit1 unit2)
+            :else         (format "one %s and %d %ss ago" unit1 amount2 unit2))
+        (cond
+            (= amount2 0) (format "%d %ss ago" amount1 unit1)
+            (= amount2 1) (format "%d %ss and one %s ago" amount1 unit1 unit2)
+            :else         (format "%d %ss and %d %ss ago" amount1 unit1 amount2 unit2))))
+
