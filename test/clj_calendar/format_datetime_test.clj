c3e922b416554e24ff0986843b5a093d0701c17f
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-01-22 09:35:08 +0100

    Stub for new test: format_datetime_test

---
+++ test/clj_calendar/format_datetime_test.clj
@@ -0,0 +1,29 @@
+;
+;  (C) Copyright 2015, 2016  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+(ns clj-calendar.format-datetime-test
+    (:require [clojure.test :refer :all]
+              [clj-calendar.calendar :refer :all]))
+
+
+;
+; Common functions used by tests.
+;
+
+(defn callable?
+    "Test if given function-name is bound to the real function."
+    [function-name]
+    (clojure.test/function? function-name))
+
+
+
+
