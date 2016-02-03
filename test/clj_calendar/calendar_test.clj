7e29505366d2bf6bd2c4fb18ecb578ce833700da
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-02-03 21:40:44 +0100

    New test: test-get-calendar-3

--- test/clj_calendar/calendar_test.clj
+++ test/clj_calendar/calendar_test.clj
@@ -142,3 +142,19 @@
             (is (=  (.get calendar (java.util.Calendar/DAY_OF_MONTH)) 1)))
 ))
 
+(deftest test-get-calendar-3
+    "Check the function clj-calendar.calendar.calendar/get-calendar."
+    (testing "the function clj-calendar.calendar.calendar/get-calendar." 
+        (is (not (nil? (get-calendar))))
+        (is (>= (.get (get-calendar) (java.util.Calendar/HOUR)) 0))
+        (is (<= (.get (get-calendar) (java.util.Calendar/HOUR)) 11))
+        (is (>= (.get (get-calendar) (java.util.Calendar/HOUR_OF_DAY)) 0))
+        (is (<= (.get (get-calendar) (java.util.Calendar/HOUR_OF_DAY)) 23))
+        (is (>= (.get (get-calendar) (java.util.Calendar/MINUTE)) 0))
+        (is (<= (.get (get-calendar) (java.util.Calendar/MINUTE)) 59))
+        (is (>= (.get (get-calendar) (java.util.Calendar/SECOND)) 0))
+        (is (<= (.get (get-calendar) (java.util.Calendar/SECOND)) 59))
+        (is (>= (.get (get-calendar) (java.util.Calendar/MILLISECOND)) 0))
+        (is (<= (.get (get-calendar) (java.util.Calendar/MILLISECOND)) 999))
+))
+
