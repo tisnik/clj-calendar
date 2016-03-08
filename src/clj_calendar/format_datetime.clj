6421bfe5fc29f7d22282098549b28e2920291a19
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-03-08 12:00:11 +0100

    Refactoring: use predicates zero? and one? instead of ugly comparators

--- src/clj_calendar/format_datetime.clj
+++ src/clj_calendar/format_datetime.clj
@@ -13,6 +13,11 @@
 (ns clj-calendar.format-datetime
     "Namespace that contains various functions for handling calendar and dates.")
 
+(defn one?
+    "Predicate that is especially useful in this namespace."
+    [n]
+    (== n 1))
+
 (defn format-time
     "Format a given time unit into human readable format."
     [amount unit]
@@ -21,15 +26,15 @@
 (defn format-time-
     "Format time into human readable format."
     [amount1 amount2 unit1 unit2]
-    (if (= amount1 1)
+    (if (one? amount1)
         (cond
-            (= amount2 0) (format "one %s ago" unit1)
-            (= amount2 1) (format "one %s and one %s ago" unit1 unit2)
-            :else         (format "one %s and %d %ss ago" unit1 amount2 unit2))
+            (zero? amount2) (format "one %s ago" unit1)
+            (one? amount2)  (format "one %s and one %s ago" unit1 unit2)
+            :else           (format "one %s and %d %ss ago" unit1 amount2 unit2))
         (cond
-            (= amount2 0) (format "%d %ss ago" amount1 unit1)
-            (= amount2 1) (format "%d %ss and one %s ago" amount1 unit1 unit2)
-            :else         (format "%d %ss and %d %ss ago" amount1 unit1 amount2 unit2))))
+            (zero? amount2) (format "%d %ss ago" amount1 unit1)
+            (one? amount2)  (format "%d %ss and one %s ago" amount1 unit1 unit2)
+            :else           (format "%d %ss and %d %ss ago" amount1 unit1 amount2 unit2))))
 
 
 (defn format-month
@@ -38,9 +43,9 @@
     (if (> months 1)
         (format "%d months ago" months)
         (cond
-            (== days 0) "one month ago"
-            (>  days 1) (format "one month and %d days ago" days)
-            :else       (format "one month and one day ago"))))
+            (zero? days) "one month ago"
+            (>  days 1)  (format "one month and %d days ago" days)
+            :else        (format "one month and one day ago"))))
 
 (defn format-day
     "Format days/hours in case the given build is younger than one month."
@@ -60,7 +65,7 @@
 (defn format-seconds
     "Format seconds into human readable format."
     [seconds]
-    (if (= seconds 1)
+    (if (one? seconds)
         (format "one second ago")
         (format "%d seconds ago" seconds)))
 
