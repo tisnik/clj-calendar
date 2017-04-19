07acb7db77787271ec5844a88a4691abb483a857
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-04-19 13:57:50 +0200

    Two tests for function get-calendar

--- test/clj_calendar/calendar_test.clj
+++ test/clj_calendar/calendar_test.clj
@@ -1,5 +1,5 @@
 ;
-;  (C) Copyright 2015, 2016  Pavel Tisnovsky
+;  (C) Copyright 2015, 2016, 2017  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -11,7 +11,7 @@
 ;
 
 (ns clj-calendar.calendar-test
-    (:require [clojure.test :refer :all]
+    (:require [clojure.test          :refer :all]
               [clj-calendar.calendar :refer :all]))
 
 
@@ -176,6 +176,25 @@
     (testing "the function clj-calendar.calendar/get-calendar."
         (let [calendar (get-calendar)]
             (.set calendar 2000 01 01 10 20 30)
+            (is (>=  (.get calendar (java.util.Calendar/MILLISECOND)) 0))
+            (is (<=  (.get calendar (java.util.Calendar/MILLISECOND)) 1000)))
+))
+
+(deftest test-get-calendar-6
+    "Check the function clj-calendar.calendar/get-calendar."
+    (testing "the function clj-calendar.calendar/get-calendar."
+        (let [calendar (get-calendar)]
+            (.set calendar 0 01 01 10 20 30)
+            (is (=  (.get calendar (java.util.Calendar/HOUR)) 10))
+            (is (=  (.get calendar (java.util.Calendar/MINUTE)) 20))
+            (is (=  (.get calendar (java.util.Calendar/SECOND)) 30)))
+))
+
+(deftest test-get-calendar-7
+    "Check the function clj-calendar.calendar/get-calendar."
+    (testing "the function clj-calendar.calendar/get-calendar."
+        (let [calendar (get-calendar)]
+            (.set calendar 0 01 01 10 20 30)
             (is (>=  (.get calendar (java.util.Calendar/MILLISECOND)) 0))
             (is (<=  (.get calendar (java.util.Calendar/MILLISECOND)) 1000)))
 ))
