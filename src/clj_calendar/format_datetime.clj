bfed61464b3d25735a658119045f8e9ca7119388
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-10 08:50:56 +0200

    Updated (c) header in changed files

--- src/clj_calendar/format_datetime.clj
+++ src/clj_calendar/format_datetime.clj
@@ -1,5 +1,5 @@
 ;
-;  (C) Copyright 2015, 2016  Pavel Tisnovsky
+;  (C) Copyright 2015, 2016, 2020  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
