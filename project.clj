81f1d694ce7af74b4c64daf4f6d50bc3b55751cb
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-10 08:50:16 +0200

    Updated Clojure version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 ;
-;  (C) Copyright 2015, 2016  Pavel Tisnovsky
+;  (C) Copyright 2015, 2016, 2020  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -15,7 +15,7 @@
     :url "http://example.com/FIXME"
     :license {:name "Eclipse Public License"
               :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.6.0"]]
+    :dependencies [[org.clojure/clojure "1.8.0"]]
     :plugins [[test2junit "1.1.0"]
               [codox "0.8.11"]
               [lein-cloverage "1.0.6"]])
